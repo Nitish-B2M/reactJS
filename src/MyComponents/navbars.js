fd87eece02e4c71065d9fe95b666c9a4242610e2
Nitish-B2M <nitishssharma20@gnu.ac.in>
2022-09-28 23:15:29 +0530

    adding different different format of navbar also with color

--- src/MyComponents/navbars.js
+++ src/MyComponents/navbars.js
@@ -4,6 +4,8 @@ import './navbars.css';
 
 export const Navbars = () => {
     return(
+        <>
+
         <div className='navbar'>
             <div className="logo">
                 <div className="company_name">NY-OFFICIAL</div>
@@ -20,10 +22,153 @@ export const Navbars = () => {
                 <div className="user">
                     <a href='#'>Login</a>
                 </div>
-                <div className="fa">
-                    {/* <FontAwesomeIcon icon="fa-regular fa-user" /> */}
-                </div>  
             </div>
         </div>
+
+
+        <div className='navbar navbar1'>
+            <div className="logo">
+                <div className="company_name">NY-OFFICIAL</div>
+            </div>
+            <div className="navbar_menu">
+                <ul>
+                    <li className='listting_menu'><a href="#">Menu</a></li>
+                    <li className='listting_menu'><a href="#">About</a></li>
+                    <li className='listting_menu'><a href="#">Contact us</a></li>
+                    <li className='listting_menu'><a href="#">More</a></li>
+                </ul>
+            </div>
+            <div className='login-css'>
+                <div className="user">
+                    <a href='#'>Login</a>
+                </div>
+            </div>
+        </div>
+
+        <div className='navbar navbar2'>
+            <div className='logo_plus_menu'>
+                <div className="logo">
+                    <div className="company_name">NY-OFFICIAL</div>
+                </div>
+                <div className="navbar_menu">
+                    <ul>
+                        <li className='listting_menu'><a href="#">Menu</a></li>
+                        <li className='listting_menu'><a href="#">About</a></li>
+                        <li className='listting_menu'><a href="#">Contact us</a></li>
+                        <li className='listting_menu'><a href="#">More</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className='login-css'>
+                <div className="user">
+                    <a href='#'>Login</a>
+                </div>
+            </div>
+        </div>
+
+        <div className='navbar navbar3'>
+            <div className="logo">
+                <div className="company_name">NY-OFFICIAL</div>
+            </div>
+            <div className="menu_plus_login">
+                <div className="navbar_menu">
+                    <ul>
+                        <li className='listting_menu'><a href="#">Menu</a></li>
+                        <li className='listting_menu'><a href="#">About</a></li>
+                        <li className='listting_menu'><a href="#">Contact us</a></li>
+                        <li className='listting_menu'><a href="#">More</a></li>
+                    </ul>
+                </div>
+                <div className='login-css'>
+                    <div className="user">
+                        <a href='#'>Login</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div className='navbar navbar4'>
+            <div className="logo2">
+                <div className="company_name">NY-OFFICIAL</div>
+            </div>
+            <div className="navbar_menu">
+                <ul>
+                    <li className='listting_menu'><a href="#">Menu</a></li>
+                    <li className='listting_menu'><a href="#">About</a></li>
+                    <li className='listting_menu'><a href="#">Contact us</a></li>
+                    <li className='listting_menu'><a href="#">More</a></li>
+                </ul>
+            </div>
+            <div className='login-css'>
+                <div className="user">
+                    <a href='#'>Login</a>
+                </div>
+            </div>
+        </div>
+
+
+        <div className='navbar navbar1'>
+            <div className="logo2">
+                <div className="company_name">NY-OFFICIAL</div>
+            </div>
+            <div className="navbar_menu">
+                <ul>
+                    <li className='listting_menu'><a href="#">Menu</a></li>
+                    <li className='listting_menu'><a href="#">About</a></li>
+                    <li className='listting_menu'><a href="#">Contact us</a></li>
+                    <li className='listting_menu'><a href="#">More</a></li>
+                </ul>
+            </div>
+            <div className='login-css'>
+                <div className="user">
+                    <a href='#'>Login</a>
+                </div>
+            </div>
+        </div>
+
+        <div className='navbar navbar2'>
+            <div className='logo_plus_menu'>
+                <div className="logo2">
+                    <div className="company_name">NY-OFFICIAL</div>
+                </div>
+                <div className="navbar_menu">
+                    <ul>
+                        <li className='listting_menu'><a href="#">Menu</a></li>
+                        <li className='listting_menu'><a href="#">About</a></li>
+                        <li className='listting_menu'><a href="#">Contact us</a></li>
+                        <li className='listting_menu'><a href="#">More</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className='login-css'>
+                <div className="user">
+                    <a href='#'>Login</a>
+                </div>
+            </div>
+        </div>
+
+        <div className='navbar navbar3'>
+            <div className="logo2">
+                <div className="company_name">NY-OFFICIAL</div>
+            </div>
+            <div className="menu_plus_login">
+                <div className="navbar_menu">
+                    <ul>
+                        <li className='listting_menu'><a href="#">Menu</a></li>
+                        <li className='listting_menu'><a href="#">About</a></li>
+                        <li className='listting_menu'><a href="#">Contact us</a></li>
+                        <li className='listting_menu'><a href="#">More</a></li>
+                    </ul>
+                </div>
+                <div className='login-css'>
+                    <div className="user">
+                        <a href='#'>Login</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
     );
 }
